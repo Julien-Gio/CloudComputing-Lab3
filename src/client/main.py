8aa5f5d1bd16ec537423d7e09d12aaa2c1ff0166
Julien Giovinazzo <juliengiovinazzo@gmail.com>
2020-10-13 17:01:29 +0200

    Added the user interface + tweeks

--- src/client/main.py
+++ src/client/main.py
@@ -5,23 +5,51 @@
 
 import boto3
 from datetime import datetime
+from guietta import Gui, _, III
 
 
 def main():
-    global client
+    global client, request_queue_url, response_queue_url
     client = boto3.client('sqs')
-    
+
+    # Get the queue urls
     request_queue_url = client.get_queue_url(QueueName='request_queue')['QueueUrl']
     response_queue_url = client.get_queue_url(QueueName='response_queue')['QueueUrl']
     print("Request queue URL: " + request_queue_url)
     print("Response queue URL: " + response_queue_url)
     print('')
-    
-    while True:
-        values = input("Values (comma seperated): ")
-        send_msg(request_queue_url, values)
-        wait_for_response(response_queue_url)
 
+    gui = Gui([ "Enter numbers:", "__n1__"  , "__n2__", "__n3__", "__n4__", ["Go"] ],
+              [ III             , "__n5__"  , "__n6__", "__n7__", "__n8__", III    ],
+              [ "Result: "      , "response", _       , _       , _       , _      ])
+    
+    gui.title("Julien's Cloud Computing Lab3")
+    gui.window().resize(500, 100)
+    gui.response = "."
+
+    # Handle ui events
+    while True:
+        name, event = gui.get()
+        print(name, event)
+        if name == None:
+            # The user pressed the X (quits the app)
+            break
+        if name == 'Go':  # Calcuate the result
+            # The message must be comma seperated values
+            message = str(gui.n1) + "," + str(gui.n2) + "," + str(gui.n3) + "," +\
+                      str(gui.n4) + "," + str(gui.n5) + "," + str(gui.n6) + "," +\
+                      str(gui.n7) + "," + str(gui.n8)
+            send_msg(request_queue_url, message)
+
+            gui.response = "Waiting for response..."
+            gui.widgets["Go"].setEnabled(False)  # Disable the go button while waiting for a response from worker
+            gui.execute_in_background(  # Execute the queue monitoring funciton in a background thread as to not block the UI thread
+                wait_for_response,
+                args=[response_queue_url],
+                callback=on_response_received
+            )
+            
+    
 
 def send_msg(queue_url, msg):
     sqs_response = client.send_message(
@@ -43,13 +71,21 @@ def wait_for_response(queue_url):
         if 'Messages' in sqs_response:
             message = sqs_response['Messages'][0]
             print("Response @ " + datetime.now().strftime("%H:%M:%S") +
-                  ": " +message['Body'])
+                  ": " + message['Body'])
 
             client.delete_message(
                 QueueUrl=queue_url,
                 ReceiptHandle=message['ReceiptHandle']
             )
             break
+
+    return message['Body']
+
+
+def on_response_received(gui, response_value):
+    gui.response = response_value
+    gui.widgets["Go"].setEnabled(True)  # The worker is done, give the hand back to the user
+
 
 if __name__ == "__main__":
     main()
